9be46b8f0c43f4f58a5b25210260f4e9a07b16e7
Ryotarou Shiozawa <pekoemood@gmail.com>
2025-11-07 23:24:08 +0900

    基本型の練習

--- Main.java
+++ Main.java
@@ -1,5 +1,6 @@
 public class Main {
   public static void main(String[] args) {
-    System.out.println("Hello World");
+    System.out.println("すっきり魔王討伐");
+    System.out.println("Ver.0.1 by 楓");
   }
 }

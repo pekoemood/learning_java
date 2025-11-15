b91b159e34254591bc6f8aeb3c0534ef6c2c321b
Ryotarou Shiozawa <pekoemood@gmail.com>
2025-11-15 23:07:48 +0900

    java学習-5

---
+++ task/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+  public static void main(String[] args) {
+    System.out.println(hello("塩澤"));
+  }
+
+  public static String hello(String name) {
+    return "Hello" + name + "さん";
+  }
+}
